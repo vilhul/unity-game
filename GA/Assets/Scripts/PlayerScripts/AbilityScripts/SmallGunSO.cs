72192d002a240302b400829f604a15e96b7ad68a
vilhul <vilhul222@edu.linkoping.se>
2024-04-08 20:44:23 +0200

    pistol

--- GA/Assets/Scripts/PlayerScripts/AbilityScripts/SmallGunSO.cs
+++ GA/Assets/Scripts/PlayerScripts/AbilityScripts/SmallGunSO.cs
@@ -7,7 +7,9 @@ using UnityEngine;
 public class SmallGunSO : AbilitySO
 {
     public GameObject gunModel;
+    public GameObject bullet;
     public bool hasSpawnedGun = false;
+    public float bulletSpeed;
 
     private void Awake() {
         hasSpawnedGun = false;
@@ -20,6 +22,14 @@ public class SmallGunSO : AbilitySO
             gunModelInstance.transform.localPosition = new Vector3(0.769999981f, -0.289999992f, 0.980000019f);
             gunModelInstance.transform.Rotate(0f, 173.800003f, 0f);
             hasSpawnedGun = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Mouse0)) {
+            GameObject bulletInstance = Instantiate(bullet, player.transform.Find("Camera").position + player.transform.Find("Camera").forward, player.transform.Find("Camera").rotation);
+            Bullet bulletScript = bulletInstance.GetComponent<Bullet>();
+            bulletScript.direction = player.transform.Find("Camera").forward;
+            bulletScript.transform.Rotate(90f, 0f, 0f);
+            bulletScript.bulletSpeed = 200f;
         }
     }
 }

165a563d1922ef8a6e1f30d055faea12ee24e54f
Jocke21 <jockecode@gmail.com>
2024-04-08 22:36:10 +0200

    ändrat bullet code

--- GA/Assets/Scripts/PlayerScripts/AbilityScripts/Bullet.cs
+++ GA/Assets/Scripts/PlayerScripts/AbilityScripts/Bullet.cs
@@ -1,22 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Bullet : MonoBehaviour
+using UnityEngine.Networking;
+using Unity.Netcode;
+using Unity.Netcode.Components;
+public class Bullet : NetworkBehaviour
 {
+
+    public int damage = 10;
+
     public float bulletSpeed;
     public Rigidbody rb;
     public Vector3 direction;
 
 
     private void Start() {
-        Debug.Log(bulletSpeed);
-        rb.AddForce(1000f * bulletSpeed * Time.deltaTime * direction.normalized);
+        GetComponent<NetworkTransform>().enabled = true;
     }
 
-    private void OnCollisionEnter(Collision collision) {
+
+
+    void Update()
+    {
+        // Move the bullet locally
+        transform.Translate(Vector3.forward * bulletSpeed * Time.deltaTime);
+    }
+
+
+    /* private void OnCollisionEnter(Collision collision) {
         //här skriver vi kod för att ta skada
+        if (!isServer)
+            return;
 
+        // Check if the collision is with a player
+        PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
+        if (playerHealth != null)
+        {
+            // Apply damage to the player
+            playerHealth.TakeDamage(damage);
+        }
+
+        // Destroy the bullet after collision
+        NetworkServer.Destroy(gameObject);
         Destroy(gameObject);
-    }
+    } */
 }

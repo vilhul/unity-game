71a1afa29ec8f51991f77f94d08fdb47819baa3d
vilhul <vilhul222@edu.linkoping.se>
2024-04-10 17:32:10 +0200

    påbörjade gamemanager

--- GA/Assets/GameManager.cs
+++ GA/Assets/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -17,8 +18,11 @@ public class GameManager : MonoBehaviour
     }
 
     public GameState gameState;
+    public string currentWinner;
 
 
+    List<GameObject> inActivePlayers = new List<GameObject>();
+    List<GameObject> players = new List<GameObject>();
 
     public static GameManager Instance;
 
@@ -42,6 +46,43 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if(gameState == GameState.shopping)
+        players = GameObject.FindGameObjectsWithTag("Player").ToList<GameObject>();
+
+
+
+        switch(gameState) {
+            case GameState.menu:
+
+                break;
+            case GameState.starting:
+                break;
+            case GameState.playing:
+                Debug.Log(players.Count);
+                Debug.Log(inActivePlayers.Count);
+                foreach (GameObject player in players) {
+                    PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+                    if(playerHealth.currentHealth.Value <= 0) {
+                        inActivePlayers.Add(player);
+                        player.SetActive(false);
+                    }
+                }
+
+                if(players.Count <= 1) {
+                    foreach (GameObject player in players) {
+                        //currentWinner = player;
+                        player.SetActive(false);
+                    }
+                    gameState = GameState.ending; break;
+                }
+
+                break;
+            case GameState.ending:
+                Debug.Log(players[0]. + " won!");
+
+
+                break;
+            case GameState.shopping:
+                break;
+        }
     }
 }
